0e8722b28d0ae3e50271da57e1ae37900388095b
johnpjust <johnpjust@gmail.com>
2019-12-17 01:33:02 -0600

    updates

--- deepClassifier.py
+++ deepClassifier.py
@@ -4,66 +4,37 @@ import os
 import tensorflow as tf
 from tensorflow.keras import layers, Model, Input
 from skimage import io
+from lr_scheduler import *
+from contextlib import redirect_stdout
+import datetime
+from sklearn import metrics
+import pandas as pd
 
-def train(model, optimizer, scheduler, data_loader_train, data_loader_val, data_loader_test, args, empty_logs_train, empty_logs_val, empty_logs_test):
+def train(model, optimizer, scheduler, data_loader_train, data_loader_val, data_loader_test, args):
     epoch = args.start_epoch
     for epoch in range(args.start_epoch, args.start_epoch + args.epochs):
 
-        # print("train")
-        # for ind in range(len(data_loader_train)):
         for x_mb, y_mb in data_loader_train:
-            # x_mb = tf.signal.frame(data_loader_train[ind][0], args.num_frames, 1, axis=0)
-            # x_mb = np.array(list(more_itertools.windowed(data_loader_train[ind][0], n=args.num_frames, step=1)))
-            # x_mb = windowed(data_loader_train[ind][0], n=args.num_frames, step=1)
-            # y_mb = data_loader_train[ind][1]
-            for i_ in range(2):
-                if i_ == 0:
-                    x_mb = windowed(data_loader_train[ind][0], n=args.num_frames, step=1)
-                    y_mb = data_loader_train[ind][1]
-                elif i_ == 1:
-                    if np.random.choice(a=[False, True]):
-                        x_mb = windowed(data_loader_train[ind][0][empty_logs_train[ind] == 0], n=args.num_frames, step=1)
-                        y_mb = 0
-                    else:
-                        x_mb = np.repeat(data_loader_train[0][0], args.num_frames, axis=-1)
-                        y_mb = 0
-                    # x_mb = windowed(data_loader_train[ind][0][empty_logs_train[ind] == 0], n=args.num_frames, step=1)
-                    # y_mb = 0
-                # else:
-                #     x_mb = np.repeat(data_loader_train[0][0], args.num_frames, axis=-1)
-                #     y_mb = 0
-
-                count = 0
-                grads = [np.zeros_like(x) for x in model.trainable_variables]
-                for x_ in batch(x_mb, args.batch_size):
-                    with tf.GradientTape() as tape:
-                        count_ = tf.reduce_sum(model(x_, training=True))
-                    count += count_
-                    grads_ = tape.gradient(count_, model.trainable_variables)
-                    grads = [x1 + x2 for x1, x2 in zip(grads, grads_)]
-                grads = [None if grad is None else tf.clip_by_norm(grad, clip_norm=args.clip_norm) for grad in grads]
-                loss = count-y_mb
-                ## scale gradients by log length or number of batches (else longer logs will be weighted unduly)
-                # globalstep = optimizer.apply_gradients(zip([2*loss*x/np.float32(x_mb.shape[0]) for x in grads], model.trainable_variables))
-                globalstep = optimizer.apply_gradients(zip([2*loss*x for x in grads], model.trainable_variables))
-
-                if i_ == 0:
-                    tf.summary.scalar('loss/train', loss**2, globalstep)
+            with tf.GradientTape() as tape:
+                loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y_mb, model(x_mb, training=True)))
+            grads = tape.gradient(loss, model.trainable_variables)
+            grads = [None if grad is None else tf.clip_by_norm(grad, clip_norm=args.clip_norm) for grad in grads]
+            globalstep = optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            tf.summary.scalar('loss/train', loss, globalstep)
 
         ## potentially update batch norm variables manuallu
         ## variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='batch_normalization')
 
-        # train_loss = tf.reduce_mean(train_loss)
         validation_loss = []
         for x_mb, y_mb in data_loader_val:
-            loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y_mb, model(x_, training=False))).numpy()
+            loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y_mb, model(x_mb, training=False))).numpy()
             validation_loss.append(loss)
         validation_loss = tf.reduce_mean(validation_loss)
         # print("validation loss:  " + str(validation_loss))
 
         test_loss=[]
         for x_mb, y_mb in data_loader_test:
-            loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y_mb, model(x_, training=False))).numpy()
+            loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(y_mb, model(x_mb, training=False))).numpy()
             test_loss.append(loss)
 
         test_loss = tf.reduce_mean(test_loss)
@@ -80,6 +51,11 @@ def train(model, optimizer, scheduler, data_loader_train, data_loader_val, data_
         if stop:
             break
 
+def load_model(args, root, load_start_epoch=False):
+    # def f():
+    print('Loading model..')
+    root.restore(tf.train.latest_checkpoint(args.load or args.path))
+
 class parser_:
     pass
 
@@ -87,16 +63,19 @@ args = parser_()
 args.device = '/gpu:0'  # '/gpu:0'
 args.learning_rate = np.float32(1e-2)
 args.clip_norm = 0.1
-args.batch_size = 200 ## 6/50,
+args.batch_size = 500 ## 6/50,
 args.epochs = 5000
 args.patience = 20
 args.load = ''
-args.tensorboard = r'D:\AlmacoEarCounts\Tensorboard'
+args.tensorboard = r'D:\pycharm_projects\AWSgeo\Tensorboard'
 args.early_stopping = 500
 args.manualSeed = None
 args.manualSeedw = None
 args.p_val = 0.2
-from scipy import ndimage, stats
+
+
+args.path = os.path.join(args.tensorboard,
+                         'model_{}'.format(str(datetime.datetime.now())[:-7].replace(' ', '-').replace(':', '-')))
 
 ########### import and preprocess data
 # files = glob(r'C:\Users\mlworker\Downloads\geological_similarity\geological_similarity\**\*.jpg', recursive=True)
@@ -116,7 +95,8 @@ labels = np.load(r'D:\pycharm_projects\AWSgeo\labels.npy')
 bg = np.load(r'D:\pycharm_projects\AWSgeo\bg.npy')/255.0
 data = data - bg
 data = data.astype(np.float32)
-labels = np.unique(labels, return_inverse=True)[1].astype(np.float32)
+# label_names = np.unique(labels, return_inverse=True)[0]
+labels = np.unique(labels, return_inverse=True)[1]
 
 data_split_ind = np.random.permutation(len(data))
 data_loader_train = data[data_split_ind[:int((1-2*args.p_val)*len(data_split_ind))]]
@@ -127,14 +107,13 @@ data_loader_test = data[data_split_ind[int((1-args.p_val)*len(data_split_ind)):]
 test_labels = labels[data_split_ind[int((1-args.p_val)*len(data_split_ind)):]]
 
 train_dataset = tf.data.Dataset.from_tensor_slices((data_loader_train, train_labels))
-train_dataset = train_dataset.shuffle(buffer_size=data_loader_train.shape[0].numpy()).batch(args.batch_size)
+train_dataset = train_dataset.shuffle(buffer_size=data_loader_train.shape[0]).batch(args.batch_size)
 
-train_dataset = tf.data.Dataset.from_tensor_slices((data_loader_train, train_labels))
-train_dataset = train_dataset.batch(2*args.batch_size)
+val_dataset = tf.data.Dataset.from_tensor_slices((data_loader_val, val_labels))
+val_dataset = val_dataset.batch(2*args.batch_size)
 
 test_dataset = tf.data.Dataset.from_tensor_slices((data_loader_test, test_labels))
 test_dataset = test_dataset.batch(2*args.batch_size)
-
 
 ########### setup GPU
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -153,7 +132,7 @@ if gpus:
 ######### Create Model
 actfun = tf.nn.relu
 with tf.device(args.device):
-    inputs = tf.keras.Input(shape=(108, 192, 3), name='img') ## (108, 192, 3)
+    inputs = tf.keras.Input(shape=data.shape[1:], name='img') ## (108, 192, 3)
     x = layers.Conv2D(32, 7, activation=actfun, strides=2)(inputs)
     block_output = layers.MaxPooling2D(3, strides=2)(x)
 
@@ -161,16 +140,16 @@ with tf.device(args.device):
     x = layers.Conv2D(32, 3, activation=None, padding='same')(x)
     x = layers.add([x, block_output])
     x = layers.Conv2D(32, 1, activation=actfun)(x)
-    block_output = layers.AveragePooling2D(pool_size=2, strides=2)(x)
+    # block_output = layers.AveragePooling2D(pool_size=2, strides=2)(x)
 
-    x = layers.Conv2D(32, 1, activation=actfun, padding='same')(block_output)
-    x = layers.Conv2D(32, 3, activation=None, padding='same')(x)
-    x = layers.add([x, block_output])
-    x = layers.Conv2D(32, 1, activation=actfun)(x)
-    x = layers.AveragePooling2D(2, strides=2)
+    # x = layers.Conv2D(32, 1, activation=actfun, padding='same')(block_output)
+    # x = layers.Conv2D(32, 3, activation=None, padding='same')(x)
+    # x = layers.add([x, block_output])
+    # x = layers.Conv2D(32, 1, activation=actfun)(x)
+    # block_output = layers.AveragePooling2D(2, strides=2)(x)
 
-    x = layers.Conv2D(32, 1, activation=actfun)(block_output)
-    x = layers.Conv2D(32, 3, activation=None)(x)
+    # x = layers.Conv2D(32, 1, activation=actfun)(block_output)
+    # x = layers.Conv2D(32, 3, activation=None)(x)
     x = layers.GlobalAveragePooling2D()(x)
 
     x = layers.Flatten()(x)
@@ -179,7 +158,6 @@ with tf.device(args.device):
     model = tf.keras.Model(inputs, logits, name='toy_resnet')
     model.summary()
 
-
 writer = tf.summary.create_file_writer(args.path)
 writer.set_as_default()
 tf.compat.v1.train.get_or_create_global_step()
@@ -187,11 +165,10 @@ tf.compat.v1.train.get_or_create_global_step()
 # global_step = tf.compat.v1.train.get_global_step()
 # global_step.assign(0)
 
-root = None
 args.start_epoch = 0
 
 print('Creating optimizer..')
-with tf.device(device):
+with tf.device(args.device):
     optimizer = tf.optimizers.Adam()
 root = tf.train.Checkpoint(optimizer=optimizer,
                            model=model,
@@ -209,4 +186,56 @@ with open(os.path.join(args.path, 'modelsummary.txt'), 'w') as f:
         model.summary()
 
 with tf.device(args.device):
-    train(model, optimizer, scheduler, data_loader_train, data_loader_val, data_loader_test, args, empty_logs_train, empty_logs_val, empty_logs_test)
+    train(model, optimizer, scheduler, train_dataset, val_dataset, test_dataset, args)
+
+
+################# check classification accuracy ##############################
+test_x_mb = []
+test_y_mb = []
+for x_mb, y_mb in test_dataset:
+    test_x_mb.append(tf.nn.softmax(model(x_mb, training=False)).numpy())
+    test_y_mb.append(y_mb)
+test_x_mb = np.argmax(np.concatenate(test_x_mb), axis=1)
+test_y_mb = np.concatenate(test_y_mb)
+
+#### confusion matrix
+conf_mat = metrics.confusion_matrix(test_y_mb.astype(np.float32), test_x_mb) ## tree methods tend to have higher false negative rates than ANN
+cols = ['andesite', 'gneiss', 'marble', 'quartzite', 'rhyolite', 'schist']
+df = pd.DataFrame(conf_mat/np.expand_dims(np.sum(conf_mat, axis=1), axis=1), columns=cols, index=cols)
+print(df)
+
+########################## get logit embeddings for kNN search ########################
+test_x_mb = []
+test_y_mb = []
+for x_mb, y_mb in test_dataset:
+    test_x_mb.append(model(x_mb, training=False).numpy())
+    test_y_mb.append(y_mb)
+test_x_mb = np.concatenate(test_x_mb)
+test_y_mb = np.concatenate(test_y_mb)
+np.savetxt(r'D:\pycharm_projects\AWSgeo\Tensorboard\model_2019-12-17-00-38-33\test_logits.csv', test_x_mb, delimiter=',')
+np.savetxt(r'D:\pycharm_projects\AWSgeo\Tensorboard\model_2019-12-17-00-38-33\test_labels.csv', test_y_mb, delimiter=',')
+
+val_x_mb = []
+val_y_mb = []
+for x_mb, y_mb in val_dataset:
+    val_x_mb.append(model(x_mb, training=False).numpy())
+    val_y_mb.append(y_mb)
+val_x_mb = np.concatenate(val_x_mb)
+val_y_mb = np.concatenate(val_y_mb)
+np.savetxt(r'D:\pycharm_projects\AWSgeo\Tensorboard\model_2019-12-17-00-38-33\val_logits.csv', val_x_mb, delimiter=',')
+np.savetxt(r'D:\pycharm_projects\AWSgeo\Tensorboard\model_2019-12-17-00-38-33\val_labels.csv', val_y_mb, delimiter=',')
+
+train_x_mb = []
+train_y_mb = []
+for x_mb, y_mb in train_dataset:
+    train_x_mb.append(model(x_mb, training=False).numpy())
+    train_y_mb.append(y_mb)
+train_x_mb = np.concatenate(train_x_mb)
+train_y_mb = np.concatenate(train_y_mb)
+np.savetxt(r'D:\pycharm_projects\AWSgeo\Tensorboard\model_2019-12-17-00-38-33\train_logits.csv', train_x_mb, delimiter=',')
+np.savetxt(r'D:\pycharm_projects\AWSgeo\Tensorboard\model_2019-12-17-00-38-33\train_labels.csv', train_y_mb, delimiter=',')
+
+#### tensorboard --logdir=D:\pycharm_projects\AWSgeo\Tensorboard
+
+########### C:\Program Files\NVIDIA Corporation\NVSMI
+######### nvidia-smi  -l 2
